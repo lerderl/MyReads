6886545a7da4ccec355a7af90fbcc60a952774d1
Joseph Olukunle <josepholukunle1107@gmail.com>
2022-05-24 14:39:27 +0100

    Added static views of the shelves

--- starter/src/App.js
+++ starter/src/App.js
@@ -1,9 +1,11 @@
 // import { useState } from "react";
 
 import "./App.css";
-
+import Read from "./components/Read";
 import Header from "./components/Header";
 import SearchBook from "./components/SearchBook";
+import WantToRead from "./components/WantToRead";
+import CurrentlyReading from "./components/CurrentlyReading";
 
 function App() {
   // const [showSearchPage, setShowSearchpage] = useState(false);
@@ -12,6 +14,13 @@ function App() {
     <div className="app">
       <div className="list-books">
         <Header />
+        <div className="list-books-content">
+          <div>
+            <WantToRead />
+            <CurrentlyReading />
+            <Read />
+          </div>
+        </div>
         <SearchBook />
       </div>
     </div>
