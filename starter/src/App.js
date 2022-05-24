653eed3c949b9d9fd585a1f5acdcdde1defe14df
Joseph Olukunle <josepholukunle1107@gmail.com>
2022-05-24 14:17:02 +0100

    Added button to search for books

--- starter/src/App.js
+++ starter/src/App.js
@@ -1,7 +1,9 @@
+// import { useState } from "react";
+
 import "./App.css";
-import { useState } from "react";
 
 import Header from "./components/Header";
+import SearchBook from "./components/SearchBook";
 
 function App() {
   // const [showSearchPage, setShowSearchpage] = useState(false);
@@ -10,6 +12,7 @@ function App() {
     <div className="app">
       <div className="list-books">
         <Header />
+        <SearchBook />
       </div>
     </div>
   );
---
+++ starter/src/components/SearchBook.js
@@ -0,0 +1,11 @@
+// import { Link } from 'react-router-dom';
+
+const SearchBook = () => {
+  return (
+    <div className="open-search">
+      Add a book
+    </div>
+  );
+};
+
+export default SearchBook;
